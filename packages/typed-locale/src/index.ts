613d69be4e52ae5558295962ffc42633aeda9511
Simon Boisset <simon@lezo.dev>
2025-09-07 23:04:00 +0200

    refacto(deferred): Remove zod utils

--- packages/typed-locale/src/index.ts
+++ packages/typed-locale/src/index.ts
@@ -2,7 +2,6 @@ export type {DeferredTranslationPayload} from './deferred-translation';
 export type {InferPartialTranslation, InferTranslation} from './infer';
 export type {OptionsGenerator, TranslationOption} from './options';
 export type {Translator} from './translator';
-export type {TranslationIssue} from './zod-utils';
 
 export {createDeferredTranslator, createOptionDeferrer, isDeferredTranslation} from './deferred-translation';
 export {createLazyTranslator} from './lazy-translator';
@@ -12,4 +11,3 @@ export {select} from './select';
 export {createTranslator} from './translator';
 export {createTranslatorFromDictionary} from './translator-from-dictionary';
 export {getTranslatorScope} from './translator-scope';
-export {createZodTranslator} from './zod-utils';
